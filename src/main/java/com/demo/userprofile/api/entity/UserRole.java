1c2642ed5c1a6ea18f95e3fa296c5da843cd771a
Solomon Alexander <solomonprathap@gmail.com>
2018-06-15 17:31:44 +1000

    code changed to add persistence

--- src/main/java/com/demo/userprofile/api/entity/UserRole.java
+++ src/main/java/com/demo/userprofile/api/entity/UserRole.java
@@ -20,18 +20,50 @@ public class UserRole {
 	private Long id;
 
 	@Column(name = "USER_ID")
-	private String userID;
+	private String userId;
 	
 	@Column(name = "ROLE_ID")
-	private String roleID;
+	private String roleId;
 	
 	@ManyToOne
-	@JoinColumns({@JoinColumn(name="USER_ID", referencedColumnName = "USER_ID"),@JoinColumn(name="ROLE_ID", referencedColumnName = "ROLE_ID")})
+	@JoinColumns({@JoinColumn(name="userId", referencedColumnName = "userId"),@JoinColumn(name="roleId", referencedColumnName = "roleId")})
 	private User user;
 	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserID(String userId) {
+		this.userId = userId;
+	}
+
+	public String getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(String roleId) {
+		this.roleId = roleId;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", userID=" + userID + ", roleID=" + roleID + "]";
+		return "User [id=" + id + ", userId=" + userId + ", roleId=" + roleId + "]";
 	}
 
 }
